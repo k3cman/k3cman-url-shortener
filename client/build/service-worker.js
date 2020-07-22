7b3eed86e3c2e43c2bb2baacc033e863407e3167
k3cman <kecman.dev@gmail.com>
2020-07-23 00:01:30 +0200

    remove build from commits

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68ccfaf0522b0adb06dd1d2ac9a3a6b8.js"
+  "/precache-manifest.cf2a26e3ec0b025f4c488609e417b924.js"
 );
 
 self.addEventListener('message', (event) => {
