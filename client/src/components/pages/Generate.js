fa3f8b970e22b269c657f5902957e5ff5c7b2930
k3cman <kecman.dev@gmail.com>
2020-07-22 23:46:06 +0200

    add yup for validation on front

--- client/src/components/pages/Generate.js
+++ client/src/components/pages/Generate.js
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import * as yup from "yup";
 
 export const Generate = () => {
+  const [url, setUrl] = useState(123);
+  const [handle, setHandle] = useState("");
+
+  const schema = yup.object().shape({
+    url: yup.string().trim().url().required(),
+    handle: yup
+      .string()
+      .trim()
+      .matches(/^[\w\-]+$/i)
+      .nullable(),
+  });
+
+  const changeHandle = (e) => {
+    setHandle(e.target.value);
+  };
+
+  const changeUrl = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log({
+      url: url,
+      handle: handle,
+    });
+    schema.validate({ url, handle }).then(
+      () => {
+        console.log("success");
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  };
+
   return (
     <div>
       <form>
-        <label for="basic-url">Your URL *</label>
+        <label htmlFor="basic-url">Your URL *</label>
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="basic-url"
           aria-describedby="basic-addon3"
+          onChange={(e) => changeUrl(e)}
         />
-        <label for="basic-url">Handle</label>
-        <input
-          type="text"
-          class="form-control"
-          id="handle"
-          aria-describedby="basic-addon3"
-        />
-        <button type="submit" id="submit" class="btn btn-primary">
+        <label htmlFor="basic-url">Handle</label>
+        <input name="basic-url" type="text" onChange={(e) => changeHandle(e)} />
+        <button
+          type="submit"
+          id="submit"
+          className="btn btn-primary"
+          onClick={(e) => submitForm(e)}
+        >
           Submit
         </button>
       </form>
